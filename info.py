a782c9f12b365125196173b4812cadb60d192356
viroyal455 <royalvivekcreation@gmail.com>
2025-01-12 22:31:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7604973572:AAFLkqMFiEPNBLo49xhEW1cQY-IE48m
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5553150886').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Royal_Vivek17") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/+K2kgCBgaat80YWQ9") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002359972466'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/viroyal')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002366307373').split()]
